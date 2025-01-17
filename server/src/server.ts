e7780434876271d25212888da8ca3f22ccb20572
Fortunejite <fortunejite1234@gmail.com>
2025-01-17 08:01:03 +0100

    mongodb configured

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import app from "./app";
 import dotenv from 'dotenv'
+import dbConnect from "./configs/db";
 
 dotenv.config()
 
 const PORT = process.env.PORT || 5000;
+
+dbConnect()
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
